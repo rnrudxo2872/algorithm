7502d726099a99b5c2a6fa20578a32310b17ec44
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2021-02-14 18:43:50 +0900

    9663 result

--- BackJun_9663.cpp
+++ BackJun_9663.cpp
@@ -7,16 +7,20 @@ int col[16] = { 0, };
 int N, ans;
 
 bool possible(int x) { // 퀸을 놓을 수 있는지 판단하는 function
-
+	for (int i = 1; i < x; i++) {
+		if (col[i] == col[x] || abs(x - i) == abs(col[x] - col[i]))
+			return false;
+	}
+	return true;
 }
 
 void qeen(int x) {
-	if (x == N)
+	if (x == N + 1)
 		ans++;
 	else {
 		for (int i = 1; i <= N; i++) {
 			col[x] = i;
-			if (possible(i))
+			if (possible(x))
 				qeen(x + 1);
 		}
 	}
@@ -28,5 +32,7 @@ int main() {
 
 	cin >> N;
 
-	qeen(0);
+	qeen(1);
+
+	cout << ans;
 }
