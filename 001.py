735541639ffd3f99742480f9b7d4af54969b3803
gyeongkoo <rnrudxo2872@naver.com>
2023-10-31 21:12:37 +0900

    수정

--- 001.py
+++ 001.py
@@ -1,12 +1,3 @@
 # 뒤에서 5등 위로
 def solution(num_list):
-    answer = num_list
-    answer.sort()
-    answer.reverse()
-    
-    for i in range(0, 5):
-        answer.pop()
-        
-    answer.reverse()
-    
-    return answer
+    return sorted(num_list)[5:]
