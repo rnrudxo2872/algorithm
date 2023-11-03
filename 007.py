5c11eeef0621a5ac82b704307a4d8d96d8cde698
gyeongkoo <rnrudxo2872@naver.com>
2023-11-03 23:47:02 +0900

    연습7

---
+++ 007.py
@@ -0,0 +1,47 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/169199
+
+def bfs(board, ri, ci):
+    visit = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
+    delPos = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    queue = [[ri, ci, 0]];
+    
+    while queue:
+        [row, col, count] = queue.pop(0)
+        visit[row][col] = True
+        print(row, col, count, board[row][col])
+        
+        if board[row][col] == "G":
+            return count
+        
+        for [delRow, delCol] in delPos:
+            rowPos = row 
+            colPos = col 
+            while True:
+                rowPos += delRow
+                colPos += delCol
+                if rowPos < 0 or rowPos >= len(board) or colPos < 0 or colPos >= len(board[0]) or board[rowPos][colPos] == "D":
+                    rowPos -= delRow
+                    colPos -= delCol
+                    break
+            if visit[rowPos][colPos]:
+                continue
+                
+            queue.append([rowPos,colPos,count+1])
+            
+    return -1
+        
+
+def solution(board):
+    answer = 0
+    
+    isStart = False
+    for ri, row in enumerate(board):
+        for ci, col in enumerate(row):
+            if board[ri][ci] == "R":
+                isStart = True
+                answer = bfs(board, ri, ci)
+                break
+        if isStart:
+            break
+    
+    return answer
